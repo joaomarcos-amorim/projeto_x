110d318f3823d508839886dee375f1cd446cca43
Joao <joao.marcos.hsh@gmail.com>
2026-03-17 20:01:56 -0300

    Remove pasta da lista de monitoramento

--- questionario1/exercicio4.py
+++
@@ -1,8 +0,0 @@
-#Escreva um programa que leia os valores de dois ângulos 
-# internos de um triângulo e calcule o valor do terceiro ângulo.
-
-primeiro_angulo= int(input( 'digite o valor do primeiro angulo'))
-segundo_angulo= int(input( 'digite o valor do segundo angulo'))
-terceiro_angulo = ( 180 - (primeiro_angulo + segundo_angulo) )
-
-print ( f'o valor do terceiro angulo é de: {terceiro_angulo}°' )
--- questionario1/exercicio8.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que leia uma letra minúscula e imprima a sua letra maiúscula correspondente
-
-letra_minuscula = input('digite uma letra:')
-letra_em_maiusculo = letra_minuscula.upper()
-print(f'a letra {letra_minuscula} em maiuscula fica: {letra_em_maiusculo}')
