35842b62d7ad0453a760d40f13cf6e9e586eed3f
joaomarcos-amorim <joao.marcos.hsh@gmail.com>
2026-03-22 15:36:41 -0300

    Delete questionario2 directory

--- questionario2/exercicio1.py
+++
@@ -1,9 +0,0 @@
-#Escreva um programa que leia um número inteiro e verifique se ele é par ou ímpar.
-
-num = int(input('Digite um numero inteiro'))
-resto = num % 2 
-if (resto == 0):
-    print(f'o numero {num} é par')
-
-else:
-    print(f"o numero {num} é impar")
